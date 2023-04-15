b385568530c9334d554b0b094100ba0fbda8b763
Lorenzo2211 <lorenzosantonocito01@gmail.com>
2023-04-15 17:52:43 +0200

    Introducing javabasics._16 pakage

---
+++ src/javabasics/_16/Main.java
@@ -0,0 +1,91 @@
+package javabasics._16;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+    /**
+     * 1: Take the code below and put it into a function, read the code and see what it is.
+     * <p>
+     * What do you think the function does? Name the function appropriately and make sure
+     * <p>
+     * return the appropriate result type
+     *
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Put this into a function
+        // End
+        System.out.println(convertingOurTemperature());
+    }
+    static String convertingOurTemperature () {
+        double celsius = 30;
+        double fahrenheit = ((celsius * 9) / 5) + 32;
+        String conversionDescription = celsius + " Celsius is " + fahrenheit + " fahrenheit";
+        return conversionDescription;
+    }
+
+    /**
+     * 2: Create a function that processes and prints out all the names in the ArrayList
+     *
+     *    Make sure to put all the formatting code in the function, formatting:
+     *    1) to Upper case
+     *    2) Remove spaces at the start and the end
+     *    3) If the name contains any numbers, instead of returning the name, return "Invalid"
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        List<String> names = Arrays.asList("Alan   ", "Alice   ", "Ant0ni0   ", "  Aziz");
+
+
+        for (String name : names) {
+            //Call your function here
+            System.out.println(nameFixing(name));
+            }
+    }
+    static String nameFixing (String name) {
+        name = name.toUpperCase();
+        name = name.trim();
+
+        List<Character> number = Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+
+        for (int i = 0; i < name.length(); i++) {
+            if (number.contains(name.charAt(i))) {
+                return "Invalid";
+            }
+        }
+        return name;
+    }
+        //if (name.matches()) {
+        //    System.out.println("Invalid");
+        //} else System.out.println(name);
+
+        /**
+         * 3: Write a function that prints out every variable seen in the code. It should not return anything
+         */
+        private static void exercise3 () {
+
+            System.out.println("\nExercise 3:");
+
+            double myLunchPrice = 5.99;
+            String lunchType = "sandwich";
+            String description = "Nice big sandwich";
+            int weightInGrams = 500;
+            //Call your function here
+            printOut (myLunchPrice, lunchType, description, weightInGrams);
+        }
+
+        static void printOut (double myLunchPrice, String lunchType, String description, int weightInGrams){
+            System.out.println(myLunchPrice);
+            System.out.println(lunchType);
+            System.out.println(description);
+            System.out.println(weightInGrams);
+        }
+    }
