4fb9da1fb52a7d941fa9f77e57d81dca30641d7f
Lorenzo2211 <lorenzosantonocito01@gmail.com>
2023-05-06 17:25:36 +0200

    Introducing javaoop.advanced.exercise._5 pakage

---
+++ src/javaoopadvanced/advanced/_5/Exercises.java
@@ -0,0 +1,97 @@
+package javaoopadvanced.advanced._5;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Exercises {
+    public static void main(String[] args) {
+         exercise1();
+         exercise2();
+         exercise3();
+         exercise4();
+    }
+
+    /**
+     * 1: Uncomment the Files.createFile() line below and write a try catch block that
+     *    prints it's stack trace
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1: ");
+        // Your code here
+        try {
+            Files.createFile(Path.of("Test.txt"));
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * 2:
+     *
+     * Write a try-catch block that attempts to open a file
+     *
+     * In the catch block, print a message that informs the user that the file could not be found.
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+        // Your code here
+        String userInputFileName = "test-file.txt";
+        try {
+           Files.readString(Path.of(userInputFileName));
+        } catch (Exception noFile) {
+            System.err.println("The file could not be found");
+            noFile.printStackTrace();
+            throw new RuntimeException(noFile);
+
+        }
+    }
+
+    /**
+     * 3:
+     *
+     * Write a try-catch block that attempts to parse a string as an integer.
+     *
+     * In the catch block, print a message that informs the user that the input was not a valid integer.
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        // Your code here
+        try {
+            Integer.parseInt("house");
+        } catch (NumberFormatException notInteger) {
+            System.err.println("The input was not a valid integer");
+            notInteger.printStackTrace();
+            throw new RuntimeException(notInteger);
+        }
+    }
+
+    /**
+     * 4:
+     *
+     * Write a try block that around this print statement that attempts to divide 2 numbers
+     *
+     * Create multiple catch blocks that catch different types of exceptions, such as NumberFormatException and DivideByZeroException.
+     *
+     * In each catch block, print a message that informs the user of the specific exception that was caught and why it occurred.
+     *
+     */
+    private static void exercise4() {
+        System.out.println("\nExercise 4: ");
+        // Your code here
+        Double num1 = 10.0;
+        String num2AsString = "0.0";
+        try{
+            System.out.println(num1/Double.parseDouble(num2AsString));
+        } catch (NumberFormatException notDouble){
+            System.err.println("The input was not a valid double");
+            notDouble.printStackTrace();
+            throw new RuntimeException(notDouble);
+        } catch (ArithmeticException notDivisible){
+            System.err.println("The input was not divisible by 0");
+            notDivisible.printStackTrace();
+            throw new RuntimeException(notDivisible);
+        }
+    }
+}
